c10a17942ebceeb60f1b616e55771b46912ffd64
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-12-12 22:50:57 +0100

    handling car movement

--- pg.py
+++ pg.py
@@ -1,16 +1,18 @@
 import pygame
 
 pygame.init()
+
 display_width = 800
 display_height = 600
 
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('A bit Racey')
+
 black = (0, 0, 0)
 white = (255, 255, 255)
-red = (255, 0, 0)
 
-gameDisplay = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption("A bit Racey")
 clock = pygame.time.Clock()
+crashed = False
 carImg = pygame.image.load('racecar.png')
 
 
@@ -18,18 +20,37 @@ def car(x, y):
     gameDisplay.blit(carImg, (x, y))
 
 
-x = (display_width * 0.45)
-y = (display_height * 0.8)
+def game_loop():
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+    x_change = 0
+    car_speed = 0
 
-crashed = False
-while not crashed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-            # print(event)
-    gameDisplay.fill(white)
-    car(x, y)
-    pygame.display.update()
-    clock.tick(60)
+    while not crashed:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
+
+            ############################
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+                    ######################
+        ##
+        x += x_change
+        ##
+        gameDisplay.fill(white)
+        car(x, y)
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+game_loop()
 pygame.quit()
 quit()
